5b3f09118e5c41c6b1f6674ca84a2c3cefab8f97
Ian Eykamp <ian@eykamp.com>
2022-06-16 13:54:54 -0400

    rename variables

---
+++ csv_file_paths.py
@@ -0,0 +1,17 @@
+raw_csv_paths = [
+    "C:/dev/Air Partners/Data Analysis/data/east_boston/sn45-final-w-ML-PM.csv",
+    "C:/dev/Air Partners/Data Analysis/data/east_boston/sn46-final-w-ML-PM.csv",
+    "C:/dev/Air Partners/Data Analysis/data/east_boston/sn49-final-w-ML-PM.csv",
+    "C:/dev/Air Partners/Data Analysis/data/east_boston/sn62-final-w-ML-PM.csv",
+    "C:/dev/Air Partners/Data Analysis/data/east_boston/sn67-final-w-ML-PM.csv",
+    "C:/dev/Air Partners/Data Analysis/data/east_boston/sn72-final-w-ML-PM.csv",
+]
+
+processed_csv_paths = [
+    "C:/dev/Air Partners/Data Analysis/data/temp/sn45-final-w-ML-PM.csv",
+    "C:/dev/Air Partners/Data Analysis/data/temp/sn46-final-w-ML-PM.csv",
+    "C:/dev/Air Partners/Data Analysis/data/temp/sn49-final-w-ML-PM.csv",
+    "C:/dev/Air Partners/Data Analysis/data/temp/sn62-final-w-ML-PM.csv",
+    "C:/dev/Air Partners/Data Analysis/data/temp/sn67-final-w-ML-PM.csv",
+    "C:/dev/Air Partners/Data Analysis/data/temp/sn72-final-w-ML-PM.csv",
+]
--- filter_graph.py
+++ filter_graph.py
@@ -3,27 +3,11 @@ import plotly.express as px
 import plotly.graph_objs as go
 import numpy as np
 from time import strftime, strptime, mktime
+from csv_file_paths import raw_csv_paths, processed_csv_paths
 
 import pandas as pd
 
 class FilterGraph():
-    major_csv_paths = [
-        "C:/dev/Air Partners/Data Analysis/data/east_boston/sn45-final-w-ML-PM.csv",
-        "C:/dev/Air Partners/Data Analysis/data/east_boston/sn46-final-w-ML-PM.csv",
-        "C:/dev/Air Partners/Data Analysis/data/east_boston/sn49-final-w-ML-PM.csv",
-        "C:/dev/Air Partners/Data Analysis/data/east_boston/sn62-final-w-ML-PM.csv",
-        "C:/dev/Air Partners/Data Analysis/data/east_boston/sn67-final-w-ML-PM.csv",
-        "C:/dev/Air Partners/Data Analysis/data/east_boston/sn72-final-w-ML-PM.csv",
-    ]
-
-    minor_csv_paths = [
-        "C:/dev/Air Partners/Data Analysis/data/temp/sn45-final-w-ML-PM.csv",
-        "C:/dev/Air Partners/Data Analysis/data/temp/sn46-final-w-ML-PM.csv",
-        "C:/dev/Air Partners/Data Analysis/data/temp/sn49-final-w-ML-PM.csv",
-        "C:/dev/Air Partners/Data Analysis/data/temp/sn62-final-w-ML-PM.csv",
-        "C:/dev/Air Partners/Data Analysis/data/temp/sn67-final-w-ML-PM.csv",
-        "C:/dev/Air Partners/Data Analysis/data/temp/sn72-final-w-ML-PM.csv",
-    ]
 
     wind_dict = {'N': [0, 22.5],
                  'NE': [22.5, 67.5],
@@ -35,30 +19,31 @@ class FilterGraph():
                  'NW': [292.5, 337.5]}
 
     def __init__(self):
-        self.sensor_data = []
-        for file_id, minor_file in enumerate(FilterGraph.minor_csv_paths):
+        self.list_of_sensor_dataframes = []
+        for file_id, processed_file in enumerate(processed_csv_paths):
             # append the next sensor's worth of data to the list
-            self.sensor_data.append(self.prepare_data(FilterGraph.major_csv_paths[file_id], minor_file))
+            self.list_of_sensor_dataframes.append(self.prepare_data(raw_csv_paths[file_id], processed_file))
             pass
 
-    def prepare_data(self, major_file, minor_file):
-        minor_file_found = True
+    def prepare_data(self, raw_file, processed_file):
+        processed_file_exists = True
         try:
-            df_downsampled = pd.read_csv(minor_file, skiprows = [0, 1])
+            df_processed = pd.read_csv(processed_file, skiprows = [0, 1])
         except(FileNotFoundError):
-            minor_file_found = False
+            processed_file_exists = False
 
-        if minor_file_found:
-            cols = pd.MultiIndex.from_tuples([("row_id", ''), ("timestamp_local", ''), ("pm25", "percentile5"), ("pm25", "hourly_mean"), ("pm25", "percentile95"),
-                                            ("wind_direction_cardinal", '')])
-            df_downsampled.columns = cols
-            df_downsampled["timestamp_local"] = pd.to_datetime(df_downsampled["timestamp_local"], format = "%Y-%m-%d %H:%M:%S")
-            return df_downsampled
+        if processed_file_exists:
+            cols = pd.MultiIndex.from_tuples([("row_id", ''), ("timestamp_local", ''),
+                                              ("pm25", "percentile5"), ("pm25", "hourly_mean"), ("pm25", "percentile95"),
+                                              ("wind_direction_cardinal", '')])
+            df_processed.columns = cols
+            df_processed["timestamp_local"] = pd.to_datetime(df_processed["timestamp_local"], format = "%Y-%m-%d %H:%M:%S")
+            return df_processed
 
-        df_major = pd.read_csv(major_file)
-        df_minor = df_major.copy()[["timestamp_local", "pm25", "wind_dir"]]
-        df_minor["timestamp_local"] = pd.to_datetime(df_major["timestamp_local"], format = "%Y-%m-%dT%H:%M:%SZ")
-        df_minor["date"] = df_minor["timestamp_local"].dt.date
+        df_raw = pd.read_csv(raw_file)
+        df_processed = df_raw.copy()[["timestamp_local", "pm25", "wind_dir"]]
+        df_processed["timestamp_local"] = pd.to_datetime(df_raw["timestamp_local"], format = "%Y-%m-%dT%H:%M:%SZ")
+        df_processed["date"] = df_processed["timestamp_local"].dt.date
 
         def discrete_wind_direction(degrees, wind_dict = FilterGraph.wind_dict):
             for direction, bounds in wind_dict.items():
@@ -67,82 +52,83 @@ class FilterGraph():
                 # else: # the direction is between 337.5 and 360 degrees
             return "N"
 
-        df_minor["wind_direction_cardinal"] = df_minor["wind_dir"].apply(discrete_wind_direction)
+        df_processed["wind_direction_cardinal"] = df_processed["wind_dir"].apply(discrete_wind_direction)
 
         # continuous aggregation functions (e.g. for PM2.5)
         def percentile5(df):
             return df.quantile(0.05)
         def percentile95(df):
             return df.quantile(0.95)
-        def hourly_mean(df):
-            return df.mean()
+        # def hourly_mean(df):
+        #     return df.mean()
 
         # discrete aggregation for wind direction
         def hourly_mode(df):
             return df.mode()
 
         resample_frequency = "1H"
-        df_downsampled = df_minor.copy()
-        df_downsampled = df_downsampled.set_index("timestamp_local")
-        df_downsampled = df_downsampled.resample(resample_frequency).agg(
+        df_processed = df_processed.set_index("timestamp_local")
+        # the following line takes the bulk of the time to run. It is very slow:
+        df_processed = df_processed.resample(resample_frequency).agg(
             {"pm25": [percentile5, "mean", percentile95],
             "wind_direction_cardinal": hourly_mode}
         ).reset_index()
 
-        df_downsampled.to_csv(minor_file)
+        # store the processed and downsampled dataframe to a csv, to be read next time
+        df_processed.to_csv(processed_file)
 
-        return df_downsampled
+        return df_processed
 
     def update_figure(self, which_sensor, start_date, end_date, wind_direction):
-        print("HELLOOOOO WORLD")
-        print("WIND DIRECTION:", wind_direction)
 
-        df_downsampled = self.sensor_data[which_sensor]
+        df_processed = self.list_of_sensor_dataframes[which_sensor]
 
-        df_filtered = df_downsampled[
-            (df_downsampled["timestamp_local"].dt.date >= pd.Timestamp(start_date).date()) &
-            (df_downsampled["timestamp_local"].dt.date <= pd.Timestamp(end_date).date()  )
+        df_filtered = df_processed[
+            (df_processed["timestamp_local"].dt.date >= pd.Timestamp(start_date).date()) &
+            (df_processed["timestamp_local"].dt.date <= pd.Timestamp(end_date).date()  )
         ]
 
         if wind_direction is not None:
             df_filtered = df_filtered[
                 df_filtered["wind_direction_cardinal"] == wind_direction
             ]
+        # else: if wind_direction is None: pass
 
         fig = go.Figure([
             go.Scatter(
-                name='Average',
-                x=df_filtered["timestamp_local"],
-                y=df_filtered["pm25"]["hourly_mean"],
-                mode='lines',
-                line=dict(color='rgb(31, 119, 180)'),
+                name = 'Average',
+                x = df_filtered["timestamp_local"],
+                y = df_filtered["pm25"]["hourly_mean"],
+                mode = 'lines',
+                line = dict(color = 'rgb(31, 119, 180)'),
             ),
             go.Scatter(
-                name='95th Percentile',
-                x=df_filtered["timestamp_local"],
-                y=df_filtered["pm25"]["percentile95"],
-                mode='lines',
-                marker=dict(color="#444"),
-                line=dict(width=0),
-                showlegend=False
+                name = '95th Percentile',
+                x = df_filtered["timestamp_local"],
+                y = df_filtered["pm25"]["percentile95"],
+                mode = 'lines',
+                marker = dict(color = "#444"),
+                line = dict(width = 0),
+                showlegend = False
             ),
             go.Scatter(
-                name='5th PErcentile',
-                x=df_filtered["timestamp_local"],
-                y=df_filtered["pm25"]["percentile5"],
-                marker=dict(color="#444"),
-                line=dict(width=0),
-                mode='lines',
-                fillcolor='rgba(68, 68, 68, 0.3)',
-                fill='tonexty',
-                showlegend=False
+                name = '5th Percentile',
+                x = df_filtered["timestamp_local"],
+                y = df_filtered["pm25"]["percentile5"],
+                marker = dict(color = "#444"),
+                line = dict(width = 0),
+                mode = 'lines',
+                fillcolor = 'rgba(68, 68, 68, 0.3)',
+                fill = 'tonexty',
+                showlegend = False
             )
         ])
+
         fig.update_layout(
             yaxis_title='PM2.5',
             # title='PM2.5',
             hovermode="x",
-            margin={'t': 0},
+            margin={'t': 0}, # removes the awkward whitespace where the title used to be
         )
 
         return fig
