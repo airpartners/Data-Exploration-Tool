2e95db2b4e821bfb1e8d918a8bce4074f1aef915
Ian Eykamp <ian@eykamp.com>
2022-06-22 16:20:16 -0400

    testing differentiating file paths based on user

--- csv_file_paths.py
+++ csv_file_paths.py
@@ -1,23 +1,77 @@
-raw_csv_paths = [
-    "C:/dev/Air Partners/Data Analysis/data/east_boston/sn45-final-w-ML-PM.csv",
-    "C:/dev/Air Partners/Data Analysis/data/east_boston/sn46-final-w-ML-PM.csv",
-    "C:/dev/Air Partners/Data Analysis/data/east_boston/sn49-final-w-ML-PM.csv",
-    "C:/dev/Air Partners/Data Analysis/data/east_boston/sn62-final-w-ML-PM.csv",
-    "C:/dev/Air Partners/Data Analysis/data/east_boston/sn67-final-w-ML-PM.csv",
-    "C:/dev/Air Partners/Data Analysis/data/east_boston/sn72-final-w-ML-PM.csv",
-]
+import os
+user_profile = os.path.basename(os.path.normpath(os.environ['USERPROFILE']))
 
-processed_csv_paths = [
-    "C:/dev/Air Partners/Data Analysis/data/parquet/sn45-final-w-ML-PM.parquet",
-    "C:/dev/Air Partners/Data Analysis/data/parquet/sn46-final-w-ML-PM.parquet",
-    "C:/dev/Air Partners/Data Analysis/data/parquet/sn49-final-w-ML-PM.parquet",
-    "C:/dev/Air Partners/Data Analysis/data/parquet/sn62-final-w-ML-PM.parquet",
-    "C:/dev/Air Partners/Data Analysis/data/parquet/sn67-final-w-ML-PM.parquet",
-    "C:/dev/Air Partners/Data Analysis/data/parquet/sn72-final-w-ML-PM.parquet",
-]
+if user_profile == "ieykamp":
+    raw_csv_paths = [
+        "C:/dev/Air Partners/Data Analysis/data/east_boston/sn45-final-w-ML-PM.csv",
+        "C:/dev/Air Partners/Data Analysis/data/east_boston/sn46-final-w-ML-PM.csv",
+        "C:/dev/Air Partners/Data Analysis/data/east_boston/sn49-final-w-ML-PM.csv",
+        "C:/dev/Air Partners/Data Analysis/data/east_boston/sn62-final-w-ML-PM.csv",
+        "C:/dev/Air Partners/Data Analysis/data/east_boston/sn67-final-w-ML-PM.csv",
+        "C:/dev/Air Partners/Data Analysis/data/east_boston/sn72-final-w-ML-PM.csv",
+    ]
 
-flight_csv_dir = "C:/dev/Air Partners/Data Analysis/data/flights/raw/"
+    processed_csv_paths = [
+        "C:/dev/Air Partners/Data Analysis/data/parquet/sn45-final-w-ML-PM.parquet",
+        "C:/dev/Air Partners/Data Analysis/data/parquet/sn46-final-w-ML-PM.parquet",
+        "C:/dev/Air Partners/Data Analysis/data/parquet/sn49-final-w-ML-PM.parquet",
+        "C:/dev/Air Partners/Data Analysis/data/parquet/sn62-final-w-ML-PM.parquet",
+        "C:/dev/Air Partners/Data Analysis/data/parquet/sn67-final-w-ML-PM.parquet",
+        "C:/dev/Air Partners/Data Analysis/data/parquet/sn72-final-w-ML-PM.parquet",
+    ]
 
-processed_flight_dir = "C:/dev/Air Partners/Data Analysis/data/flights/processed/"
+    flight_csv_dir = "C:/dev/Air Partners/Data Analysis/data/flights/raw/"
 
-final_flights = "C:/dev/Air Partners/Data Analysis/data/flights/final/combined_flights.parquet"
+    processed_flight_dir = "C:/dev/Air Partners/Data Analysis/data/flights/processed/"
+
+    final_flights = "C:/dev/Air Partners/Data Analysis/data/flights/final/combined_flights.parquet"
+
+elif user_profile == "zxiong":
+    raw_csv_paths = [
+        "C:/dev/Air Partners/Data Analysis/data/east_boston/sn45-final-w-ML-PM.csv",
+        "C:/dev/Air Partners/Data Analysis/data/east_boston/sn46-final-w-ML-PM.csv",
+        "C:/dev/Air Partners/Data Analysis/data/east_boston/sn49-final-w-ML-PM.csv",
+        "C:/dev/Air Partners/Data Analysis/data/east_boston/sn62-final-w-ML-PM.csv",
+        "C:/dev/Air Partners/Data Analysis/data/east_boston/sn67-final-w-ML-PM.csv",
+        "C:/dev/Air Partners/Data Analysis/data/east_boston/sn72-final-w-ML-PM.csv",
+    ]
+
+    processed_csv_paths = [
+        "C:/dev/Air Partners/Data Analysis/data/parquet/sn45-final-w-ML-PM.parquet",
+        "C:/dev/Air Partners/Data Analysis/data/parquet/sn46-final-w-ML-PM.parquet",
+        "C:/dev/Air Partners/Data Analysis/data/parquet/sn49-final-w-ML-PM.parquet",
+        "C:/dev/Air Partners/Data Analysis/data/parquet/sn62-final-w-ML-PM.parquet",
+        "C:/dev/Air Partners/Data Analysis/data/parquet/sn67-final-w-ML-PM.parquet",
+        "C:/dev/Air Partners/Data Analysis/data/parquet/sn72-final-w-ML-PM.parquet",
+    ]
+
+    flight_csv_dir = "C:/dev/Air Partners/Data Analysis/data/flights/raw/"
+
+    processed_flight_dir = "C:/dev/Air Partners/Data Analysis/data/flights/processed/"
+
+    final_flights = "C:/dev/Air Partners/Data Analysis/data/flights/final/combined_flights.parquet"
+
+else: # if not Ian or Lauren, feel free to make a copy of this and change the file names below:
+    raw_csv_paths = [
+        "C:/dev/Air Partners/Data Analysis/data/east_boston/sn45-final-w-ML-PM.csv",
+        "C:/dev/Air Partners/Data Analysis/data/east_boston/sn46-final-w-ML-PM.csv",
+        "C:/dev/Air Partners/Data Analysis/data/east_boston/sn49-final-w-ML-PM.csv",
+        "C:/dev/Air Partners/Data Analysis/data/east_boston/sn62-final-w-ML-PM.csv",
+        "C:/dev/Air Partners/Data Analysis/data/east_boston/sn67-final-w-ML-PM.csv",
+        "C:/dev/Air Partners/Data Analysis/data/east_boston/sn72-final-w-ML-PM.csv",
+    ]
+
+    processed_csv_paths = [
+        "C:/dev/Air Partners/Data Analysis/data/parquet/sn45-final-w-ML-PM.parquet",
+        "C:/dev/Air Partners/Data Analysis/data/parquet/sn46-final-w-ML-PM.parquet",
+        "C:/dev/Air Partners/Data Analysis/data/parquet/sn49-final-w-ML-PM.parquet",
+        "C:/dev/Air Partners/Data Analysis/data/parquet/sn62-final-w-ML-PM.parquet",
+        "C:/dev/Air Partners/Data Analysis/data/parquet/sn67-final-w-ML-PM.parquet",
+        "C:/dev/Air Partners/Data Analysis/data/parquet/sn72-final-w-ML-PM.parquet",
+    ]
+
+    flight_csv_dir = "C:/dev/Air Partners/Data Analysis/data/flights/raw/"
+
+    processed_flight_dir = "C:/dev/Air Partners/Data Analysis/data/flights/processed/"
+
+    final_flights = "C:/dev/Air Partners/Data Analysis/data/flights/final/combined_flights.parquet"
