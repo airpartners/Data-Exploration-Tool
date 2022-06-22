ce8701ac49efa6a30cc8b6e1209d3360dc503c69
Lauren Xiong <78378477+Laurenx618@users.noreply.github.com>
2022-06-22 18:41:17 -0400

    added subplots

--- barchart.py
+++ barchart.py
@@ -1,14 +1,12 @@
-from statistics import mean
 from dash import Dash, dcc, html, Input, Output
-import plotly.express as px
+from plotly.subplots import make_subplots
 import pandas as pd
 import plotly.graph_objects as go
+import numpy as np
 
 app = Dash(__name__)
 
-# df_major = pd.read_csv('C:/Users/zxiong/Desktop/Olin/Air Partners/Code/sn45-final-w-ML-PM.csv') # Lauren
-df_major = pd.read_csv('C:/dev/Air Partners/Data Analysis/data/east_boston/sn45-final-w-ML-PM.csv') # Ian
-
+df_major = pd.read_csv('C:/Users/zxiong/Desktop/Olin/Air Partners/Code/sn45-final-w-ML-PM.csv')
 df = df_major.copy()
 df["timestamp_local"] = pd.to_datetime(df_major["timestamp_local"], format = "%Y-%m-%dT%H:%M:%SZ")
 df["date"] = df["timestamp_local"].dt.date
@@ -18,6 +16,11 @@ resample_frequency = '1H'
 df_downsampled = df.copy()
 df_downsampled = df_downsampled.set_index("timestamp_local")
 df_downsampled = df_downsampled.resample(resample_frequency).agg('mean').reset_index() # .mean()
+
+df_stats = pd.read_csv('C:/Users/zxiong/Desktop/Olin/Air Partners/Code/stats.csv')
+df_stats = df_stats.drop([3, 4, 5, 15, 22, 23, 24, 29, 30, 35])
+print(df_stats)
+
 
 app.layout = html.Div([
     dcc.Graph(id='bar-plot'),
@@ -31,7 +34,8 @@ app.layout = html.Div([
     ),
     html.Div([
             dcc.Dropdown(
-                ['mean', 'median'],
+                ['Mean', 'Median'],
+                'Mean',
                 id='data-stats'
             )
         ], style={'width': '48%', 'float': 'right', 'display': 'inline-block'})
@@ -41,17 +45,16 @@ app.layout = html.Div([
     Output('bar-plot', 'figure'),
     Input('my-date-picker-range', 'start_date'),
     Input('my-date-picker-range', 'end_date'),
-    Input('data-stats', 'value')
-)
+    Input('data-stats', 'value'))
 
 
 def update_figure(start_date, end_date, data_stats):
-
+   
     df_filtered = df_downsampled[
             (df_downsampled["timestamp_local"].dt.date >= pd.Timestamp(start_date).date()) &
             (df_downsampled["timestamp_local"].dt.date <= pd.Timestamp(end_date).date()  )
-        ]
-    df_filtered = df_filtered.drop(columns=['pressure', 'noise', 'co_ae', 'no2_ae', 'timediff'])
+        ]   
+    df_filtered = df_filtered.drop(columns=['pressure', 'noise', 'solar', 'no_ae', 'co_ae', 'no2_ae', 'co2', 'removeCO','timediff','flag'])
 
 
     #mean value
@@ -60,7 +63,7 @@ def update_figure(start_date, end_date, data_stats):
     data_median = df_filtered.median(axis=0)
     # #mode value
     # data_mode = df_filtered.mode(axis=0)
-
+    
     # fig = px.bar(
     #     x=['temp_box','temp_manifold','rh_manifold','solar','wind_dir','wind_speed','co','no','no2','o3','pm1','pm25','pm10','co2','bin0','bin1','bin2','bin3','bin4','bin5','no_ae', 'tmpc', 'wd', 'ws', 'correctedNO', 'removeCO', 'co.ML', 'no.ML', 'o2.ML', 'o3.ML', 'flag', 'pm1.ML', 'pm25.ML', 'pm10.ML'],
     #     y=data_mean if data_stats == 'mean' else data_median, # if data_stats == 'median' else data_mode,
@@ -68,60 +71,96 @@ def update_figure(start_date, end_date, data_stats):
     #     height=550,
     #     barmode="group",
     #     text_auto=True
-    # )
+    # ) 
 
     fig=go.Figure()
 
-    mean_sn45 = [100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, ]
-    median_sn45 = [110, 110, 110, 110, 110, 110, 110, 110, 110, 110, 110, 110, 110, 110, 110, 110, 110, 110, 110, 110, 110, 110, 110, 110, 110, 110, 110, 110, 110, 110, 110, 110, 110, 110]
-
-    fig.add_bar(x=['temp_box','temp_manifold','rh_manifold','solar','wind_dir','wind_speed','co','no','no2','o3','pm1','pm25','pm10','co2','bin0','bin1','bin2','bin3','bin4','bin5','no_ae', 'tmpc', 'wd', 'ws', 'correctedNO', 'removeCO', 'co.ML', 'no.ML', 'o2.ML', 'o3.ML', 'flag', 'pm1.ML', 'pm25.ML', 'pm10.ML'],
-    y=mean_sn45 if data_stats == 'mean' else median_sn45,
-    text=mean_sn45 if data_stats == 'mean' else median_sn45,
-    name='entire dataset mean' if data_stats == 'mean' else 'entire dataset median')
+    sn45_mean = df_stats['sn45_mean']
+    sn45_median = df_stats['sn45_median']
 
 
-    fig.add_bar(x=['temp_box','temp_manifold','rh_manifold','solar','wind_dir','wind_speed','co','no','no2','o3','pm1','pm25','pm10','co2','bin0','bin1','bin2','bin3','bin4','bin5','no_ae', 'tmpc', 'wd', 'ws', 'correctedNO', 'removeCO', 'co.ML', 'no.ML', 'o2.ML', 'o3.ML', 'flag', 'pm1.ML', 'pm25.ML', 'pm10.ML'],
-    y=data_mean if data_stats == 'mean' else data_median,
-    text=data_mean if data_stats == 'mean' else data_median,
-    name='selected data mean' if data_stats == 'mean' else 'selected data median',
-    error_y =
-        dict(
-            type = 'data', # value of error bar given in data coordinates
-            array = [50] * len(mean_sn45),
-            visible = True
-        )
-    ) # if data_stats == 'median' else data_mode
+    standdev = []
+    quotients_mean = []
+    quotients_median = []
+    error_by_mean = []
+    error_by_median = []
+
+    for i in df_downsampled.columns[1:]:
+            standdev.append(np.nanstd(df_downsampled[i])) # if means!=0.000000 else standdev_mean.append(0)
 
 
-    # for string in ['temp_box','temp_manifold','rh_manifold','solar','wind_dir','wind_speed','co','no','no2','o3','pm1','pm25','pm10','co2','bin0','bin1','bin2','bin3','bin4','bin5','no_ae', 'tmpc', 'wd', 'ws', 'correctedNO', 'removeCO', 'co.ML', 'no.ML', 'o2.ML', 'o3.ML', 'flag', 'pm1.ML', 'pm25.ML', 'pm10.ML']:
-    #     fig.add_shape(type="line",
-    #         x0=string[0],
-    #         y0=mean_sn45 if data_stats == 'mean' else median_sn45,
-    #         x1=string[+1],
-    #         y1=mean_sn45[0]+1 if data_stats == 'mean' else median_sn45[0]+1,
-    #         line=dict(color='#0000FF', width = 4, dash='dashdot'))
-
-    # i=0
-    # while i <= len(mean_sn45):
-    #     fig.add_bar(x=['temp_box','temp_manifold','rh_manifold','solar','wind_dir','wind_speed','co','no','no2','o3','pm1','pm25','pm10','co2','bin0','bin1','bin2','bin3','bin4','bin5','no_ae', 'tmpc', 'wd', 'ws', 'correctedNO', 'removeCO', 'co.ML', 'no.ML', 'o2.ML', 'o3.ML', 'flag', 'pm1.ML', 'pm25.ML', 'pm10.ML'],
-    #         y=data_mean if data_stats == 'mean' else data_median,
-    #         text=data_mean if data_stats == 'mean' else data_median,
-    #         name='selected data mean' if data_stats == 'mean' else 'selected data median',
-    #         secondary_y=True if mean_sn45[i] >= data_mean[i] else False
-    #         ) # if data_stats == 'median' else data_mode
-
-    #     i=i+1
+    for mean,means,i in zip(data_mean,sn45_mean,standdev):
+        quotients_mean.append(mean/means) if means!=0.000000 else quotients_mean.append(0)
+        error_by_mean.append(i/means) if means!=0.000000 else error_by_mean.append(0)
 
 
+    for median, medians,i in zip (data_median, sn45_median,standdev):
+        quotients_median.append(median/medians) if medians!=0.000000 else quotients_median.append(0)
+        error_by_median.append(i/medians) if medians!=0.000000 else error_by_median.append(0)
+
+
+    fig = make_subplots(rows=1, cols=2)
+
+    fig.add_trace(
+        go.Bar(
+            x=['co','no','no2','o3','pm1','pm25','pm10','bin0','bin1','bin2','bin3','bin4','bin5'],
+            y=quotients_mean[5:18] if data_stats == 'Mean' 
+            else  quotients_median[5:18],
+            text=data_mean if data_stats == 'Mean' else data_median,
+            opacity=0.9
+            # error_y =
+            #     dict(
+            #         type='data', # value of error bar given in data coordinates
+            #         array=error_by_mean[5:18] if data_stats == 'Mean' else error_by_median[5:18],
+            #         visible = True
+            #     )
+        ),
+    row=1, col=1
+
+    )
+    fig.add_trace(
+        go.Bar(
+            x=['co.ML', 'no.ML', 'o2.ML', 'o3.ML', 'pm1.ML', 'pm25.ML', 'pm10.ML'],
+            y=quotients_mean[22:] if data_stats == 'Mean' 
+            else  quotients_median[22:],
+            text=data_mean if data_stats == 'Mean' else data_median,
+            opacity=0.9
+            # error_y =
+            #     dict(
+            #         type='data', # value of error bar given in data coordinates
+            #         array=error_by_mean[22:] if data_stats == 'Mean' else error_by_median[22:],
+            #         visible = True
+            #     )
+        ),
+    row=1, col=2
+
+    )
+
+
+    # fig.add_bar(
+    #     x=['temp_box','temp_manifold','rh_manifold','wind_dir','wind_speed','co','no','no2','o3','pm1','pm25','pm10','bin0','bin1','bin2','bin3','bin4','bin5', 'tmpc', 'wd', 'ws', 'correctedNO', 'co.ML', 'no.ML', 'o2.ML', 'o3.ML', 'flag', 'pm1.ML', 'pm25.ML', 'pm10.ML'],
+    #     y=quotients_mean if data_stats == 'Mean' else  quotients_median,
+    #     text=data_mean if data_stats == 'Mean' else data_median,
+    #     name='Data Standarized by Mean' if data_stats == 'Mean' else 'Data Standarized by Median'
+    #     # error_y =
+    #     #     dict(
+    #     #         type='data', # value of error bar given in data coordinates
+    #     #         array=error_by_mean if data_stats == 'Mean' else error_by_median,
+    #     #         visible = True
+    #     #     )
+    # )
+    
+
+    # fig.add_bar(x=['temp_box','temp_manifold','rh_manifold','solar','wind_dir','wind_speed','co','no','no2','o3','pm1','pm25','pm10','bin0','bin1','bin2','bin3','bin4','bin5','tmpc', 'wd', 'ws', 'correctedNO', 'removeCO', 'co.ML', 'no.ML', 'o2.ML', 'o3.ML', 'flag', 'pm1.ML', 'pm25.ML', 'pm10.ML'],
+    # y=data_mean if data_stats == 'Mean' else data_median,
+    # text=data_mean if data_stats == 'Mean' else data_median,
+    # name='selected data mean' if data_stats == 'Mean' else 'selected data median') # if data_stats == 'median' else data_mode 
 
     fig.update_layout(barmode="overlay")
     fig.update_traces(width=0.55)
-    fig.update_layout(title_text="Change the title",
+    fig.update_layout(title_text="Bar Chart of Standarized Data",
                 title_font_size=30)
 
-
-    print(type(enumerate(mean_sn45)))
     return fig
 
 
