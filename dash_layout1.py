bba0c10f5c6289aef8c94d86241b3b4a209d33f1
Ian Eykamp <ian@eykamp.com>
2022-06-27 16:25:20 -0400

    add working buttons for 'dynamic' graph display

--- dash_layout1.py
+++ dash_layout1.py
@@ -1,197 +1,110 @@
 from dash import Dash, html, dcc
 from dash.dependencies import Input, Output
-import datetime
-
-from torch import CharStorage
-
-from filter_graph import FilterGraph # import from supporting file (contained in this repo)
-
-class GraphFrame():
-
-    # define HTML styles for text and dropdown menus. Use this to change font size, alignment, etc.
-    text_style = {
-        "display": "inline-block", # if you take this out, all successive elements will be displayed on separate lines
-        "transform": "translateY(0%)", # vertical alignment
-        "position": "relative",
-        "margin-left": "10px", # adds a horizontal space between dropdowns menus and next chunk of text
-        "font-size" : "30px",
-        "font-family": "Arial",
-        "line-height": "0%", # helps reduce the line spacing
-    }
-
-    dropdown_style = {
-        "display": "inline-block",
-        "width": "200px",
-        "height": "30px",
-        "margin-left": "10px",
-        "font-size": "20px",
-        "font-family": "Arial",
-        "line-height": "0%", # helps reduce the line spacing
-    }
-
-    date_picker_style = {
-        "display": "inline-block",
-        # "width": "200px", # not used or doesn't work
-        "height": "40px",
-        "margin-left": "10px",
-        # "font-family": "Arial", # not used or doesn't work
-        "line-height": "0%", # helps reduce the line spacing
-    }
-
-    def __init__(self, id_num, chart_type = 0) -> None:
-        self.id_num = id_num
-        self.graph_frame = html.Div(
-            [
-                html.Div(
-                    [
-                        html.Div(
-                            html.P("At "),
-                            style = self.text_style
-                        ),
-                        html.Div(
-                            [
-                                dcc.Dropdown(
-                                    options = [
-                                        {'label': 'Sensor 1', 'value': '0'},
-                                        {'label': 'Sensor 2', 'value': '1'},
-                                        {'label': 'Sensor 3', 'value': '2'},
-                                        {'label': 'Sensor 4', 'value': '3'},
-                                        {'label': 'Sensor 5', 'value': '4'},
-                                        {'label': 'Sensor 6', 'value': '5'},
-                                    ],
-                                    # note: in order to set the default value, you have to set value = {the VALUE you want}.
-                                    # Do NOT try to set value = {the LABEL you want}, e.g. value = 'Sensor 1'
-                                    value = '0', # default value
-                                    id = self.get_id("which-sensor"), # javascript id, used in @app.callback to reference this element, below
-                                    clearable = False # prevent users from deselecting all sensors
-                                ),
-                            ],
-                            style = self.dropdown_style
-                        ),
-                        html.Div(
-                            html.P(", what were the pollution levels between"),
-                            style = self.text_style
-                        ),
-
-                        html.Div(
-                            [
-                                dcc.DatePickerSingle(
-                                    display_format='MM/DD/Y',
-                                    date = datetime.date(2019, 12, 1), # default value
-                                    id = self.get_id('start-date'),
-                                ),
-                            ],
-                            style = self.date_picker_style
-                        ),
-                        html.Div(
-                            html.P(" and "),
-                            style = self.text_style
-                        ),
-                        html.Div(
-                            [
-                                dcc.DatePickerSingle(
-                                    date = datetime.date(2020, 1, 1), # default value
-                                    display_format='MM/DD/Y',
-                                    id = self.get_id('end-date'),
-                                ),
-                            ],
-                            style = self.date_picker_style
-                        ),
-                        html.Div(
-                            html.P("when the wind was blowing"),
-                            style = self.text_style
-                        ),
-                        html.Div(
-                            [
-                                dcc.Dropdown(
-                                    options = [
-                                        {'label': 'North',     'value': 'N'},
-                                        {'label': 'Northeast', 'value': 'NE'},
-                                        {'label': 'East',      'value': 'E'},
-                                        {'label': 'Southeast', 'value': 'SE'},
-                                        {'label': 'South',     'value': 'S'},
-                                        {'label': 'Southwest', 'value': 'SW'},
-                                        {'label': 'West',      'value': 'W'},
-                                        {'label': 'Northwest', 'value': 'NW'},
-                                    ],
-                                    value = 'NE', id = self.get_id("wind-direction")
-                                ),
-                            ],
-                            style = self.dropdown_style
-                        ),
-                        html.Div(
-                            html.P("?"),
-                        ),
-                    ]
-                ),
-                # Placeholder for a graph to be created.
-                # This graph will be updated in the @app.callback: update_figure function below
-                dcc.Graph(id = self.get_id('graph-to-update')),
-            ],
-            style = {'display': 'none'},
-            id = self.get_id('full-graph')
-        )
-
-    def get_id(self, id_str):
-        return id_str + "-" + str(self.id_num)
-
-    def make_visible(self):
-        self.graph_frame.style['display'] = 'block'
-
-    def make_in_visible(self):
-        self.graph_frame.style['display'] = 'none'
-
-    def add_graph_callback(self, app):
-        self.filter_graph = FilterGraph()
-
-        @app.callback(
-            Output(self.get_id('graph-to-update'), 'figure'),
-            Input(self.get_id('which-sensor'), 'value'),
-            Input(self.get_id('start-date'), 'date'),
-            Input(self.get_id('end-date'), 'date'),
-            Input(self.get_id('wind-direction'), 'value'),
-        )
-        def update_figure(which_sensor, start_date, end_date, wind_direction):
-            print(f"Graph with id {self.id_num} being called back!")
-            return self.filter_graph.update_figure(int(which_sensor), start_date, end_date, wind_direction)
+from graph_frame import GraphFrame
 
 class Page():
-    def __init__(self, app, nCharts) -> None:
+
+    chart_names = {
+        0: "Bar Chart",
+        1: "Timeseries",
+        2: "Correlation Plot",
+        # 4: "Polar Plot",
+    }
+
+    def __init__(self, app, n_charts = 10, n_chart_types = 3) -> None:
         self.app = app
-        self.chart_types = [0] * nCharts
+        self.n_charts = n_charts
+        self.n_chart_types = n_chart_types
+        self.chart_ids = [list(range(n_chart_types * i, n_chart_types * (i + 1))) for i in range(n_charts + 1)] # this works!
+        # self.chart_ids = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [9, 10, 11], [12, 13, 14], ..., [27, 28, 29], [30, 31, 32]]
+        self.button_ids = list(range(n_charts + 1))
+        # access this with self.chart_ids[chart_type][id_num]
+        self.next_id_num = 0 # prepare for the next graph to be added
+        self.layout = html.Div(children = [], id = 'main')
+        self.create_layout()
 
-        self.graph_objs = []
-        self.graphs = []
-        for id_num, chart_type in enumerate(self.chart_types):
-            graph = GraphFrame(id_num, chart_type)
-            graph.add_graph_callback(self.app)
-            self.graphs.append(graph.graph_frame)
-            self.graph_objs.append(graph)
-
-        self.next_id_num = 1 # prepare for the next graph to be added
-
-        self.button = html.Button('+', id = 'submit-val', n_clicks = 0)
-        # self.add_button_callback()
-
-        self.layout = html.Div(self.graphs + [self.button], id = 'main')
-
-        @self.app.callback(
-            Output(self.graph_objs[self.next_id_num].get_id('full-graph'), 'style'),
-            Input('submit-val', 'n_clicks'),
+    def create_button(self, chart_num, chart_type):
+        id_num = self.chart_ids[chart_num][chart_type]
+        print("Creating Button with button id", self.get_id('button', id_num))
+        button = \
+        html.Button(
+            children = self.chart_names[chart_type],
+            id = self.get_id('button', id_num),
+            n_clicks = 0
         )
-        def add_graph(n_clicks):
-            self.next_id_num += 1
-            if n_clicks == 0:
-                return {'display': 'none'}
-            return {'display': 'block'}
+        self.add_button_callback(id_num)
+        return button
 
-    def get_layout(self):
-        return self.layout
+    def add_button_callback(self, id_num):
+        self.app.clientside_callback(
+            """
+            function(n_clicks) {
+                console.log("HOwdy dandy, World! ... This is a Button!");
+                console.log("n_clicks: " + n_clicks);
+                if (n_clicks > 0) {
+                    return {'display': 'block'};
+                } else {
+                    return {'display': 'none'};
+                }
+            }
+            """,
+            Output(self.get_id('frame', id_num), 'style'),
+            Input(self.get_id('button', id_num), 'n_clicks'),
+        )
+
+    def create_button_set(self, chart_num):
+        buttons = []
+        for chart_type in range(self.n_chart_types):
+            buttons.append(self.create_button(chart_num, chart_type))
+
+        button_set = \
+        html.Div(
+            children = buttons,
+            id = self.get_id('button-set', self.button_ids[chart_num]),
+            style = {'display': 'block'}
+        )
+        self.add_button_set_callback(chart_num) # this is a list
+        return button_set
+
+    def add_button_set_callback(self, chart_num):
+        self.app.clientside_callback(
+            """
+            function(...inputs) {
+                console.log("HOwdy dandy, World! ...");
+                console.log("n_clicks: " + inputs);
+                sum = inputs.reduce(function(a, b) { return a + b; }, 0);
+                if (sum > 0) {
+                    return [{'display': 'none'}];
+                } else {
+                    return [{'display': 'block'}];
+                }
+            }
+            """,
+            Output(self.get_id('button-set', self.button_ids[chart_num]), 'style'),
+            # make it so the next button set erases the current one
+            *[Input(self.get_id('button-set', id_num), 'n_clicks') for id_num in self.chart_ids[chart_num + 1]]
+        )
+
+    def create_layout(self):
+        for chart_num in range(self.n_charts):
+            buttons = []
+
+            for chart_type in range(self.n_chart_types):
+                graph_frame = GraphFrame(self.app, self.chart_ids[chart_num][chart_type], chart_type)
+                self.layout.children.append(graph_frame.frame)
+                self.next_id_num += 1
+                # buttons.append(self.create_button(chart_num, chart_type))
+                # new_button_ids
+
+            self.layout.children.append(self.create_button_set(chart_num))
+
+    def get_id(self, id_str, id_num):
+        return id_str + "-" + str(id_num)
 
 if __name__ == '__main__':
     app = Dash(__name__) # initialize the app
 
-    p = Page(app, nCharts = 10)
-    app.layout = html.Div(p.get_layout())
+    p = Page(app)
+    app.layout = html.Div(p.layout)
 
     app.run_server(debug=True)
