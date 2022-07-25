4ca61785c2c437ef398c40b3eb37fdb63979e745
Lauren Xiong <78378477+Laurenx618@users.noreply.github.com>
2022-07-25 11:18:15 -0400

    updated filter conditions

--- R/flight_data_analysis.R
+++ R/flight_data_analysis.R
@@ -1,12 +1,47 @@
 library(arrow)
 library(tidyverse)
+library(viridis)
+# C:/dev/Air Partners/Data Analysis/data/east_boston/processed/sn45-final-w-ML-PM.parquet
 
-df_1 <- read_parquet("C:/dev/Air Partners/Data Analysis/data/east_boston/processed/sn45-final-w-ML-PM.parquet") %>%
+df_1 <- read_parquet("C:/Users/zxiong/Desktop/Olin/Air Partners/downsampled/sn45-final-w-ML-PM.parquet") %>%
   mutate(
     time = as.POSIXct(timestamp_local, format="%m/%d/%Y %H:%M", tz="UTC"),
   )
 
-df_1 %>%
-  head(500) %>%
-  ggplot(mapping = aes(x = time, y = adverse_flight_count)) +
-  geom_point()
+df_2 <- 
+  df_1 %>%
+  # head(500) %>%
+  filter(wind_direction_cardinal %in% c("W", "NW"), 
+         adverse_flight_count > 10) %>%
+  mutate(
+    doublepm = pm25.ML*2
+  )
+#
+df_2 %>%
+  ggplot(mapping = aes(
+    x = adverse_flight_count, 
+    y = no2.ML, 
+    color = no2.ML,
+    size = no2.ML
+  )) +
+  geom_point() +
+  # geom_col()
+  # geom_line()
+  geom_smooth(method=lm) +
+  scale_color_viridis_c() +
+  labs(
+    x = "adverse flights",
+    y = "no2",
+    title = "no2 versus adverse flights",
+    size = "no2",
+    color = "no2"
+  )
+#
+mylm <- df_2 %>%
+  lm(adverse_flight_count ~ time, .)
+print(mylm)
+inter <- mylm[[1]]
+slope <- mylm[[2]]
+r2 <- summary(mylm)$r.squared
+print(r2)
+
