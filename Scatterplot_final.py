21324a2d1b518d4be5a55f2eaf798cc790d6efcb
Lauren Xiong <78378477+Laurenx618@users.noreply.github.com>
2022-08-03 17:41:35 -0400

    resolved merge conflicts and renamed to main

--- Scatterplot_final.py
+++ Scatterplot_final.py
@@ -107,6 +107,7 @@ class Scatter(GraphFrame):
             fig = px.scatter(df, x=xaxis_column_name, y=yaxis_column_name,
                     trendline="ols",
                     hover_name=df.index,
+                    template=CSS.template
                     # log_x=True, size_max=15
                 )
 
